b53d29977d85c410edde91e2806aa2bebcee7bcc
ashuu021 <ashuu023@gmail.com>
2025-05-05 10:05:46 +0530

    hi

--- practice/var&dynamic.dart
+++ practice/var&dynamic.dart
@@ -1,11 +1,12 @@
 // Var and dyanamic
-void main(){
+void main() {
   // var keyword
   var name = "Anshu Rathore";
   print(name);
   // dynamic keyword
   dynamic age = 25;
   print(age);
+  print("hello");
   age = "Twenty Five";
   print(age);
   // var keyword cannot be reassigned to a different type
